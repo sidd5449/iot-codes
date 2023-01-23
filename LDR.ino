0f563c1893ce5f34beab402f3496e5daf7b6250d
Siddhesh Thakare <76845396+sidd5449@users.noreply.github.com>
2023-01-23 13:12:26 +0530

    Add files via upload

---
+++ LDR.ino
@@ -0,0 +1,19 @@
+#define sensorPin A0
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int reading = analogRead(sensorPin);
+
+
+  float voltage = reading * (5000 / 1024.0);
+
+  float temperature = voltage / 10;
+
+  Serial.print(temperature);
+  Serial.print(" \xC2\xB0"); 
+  Serial.println("C");
+
+  delay(1000);
+}
